7b827ade77cd28e7f818b1722af2b2b507ef71e6
uaenawook <820649664@qq.com>
2026-04-18 16:14:47 +0800

    Added a Date class

---
+++ study/C++/2026.04.16类和对象/Date.cpp
@@ -0,0 +1,43 @@
+﻿#include"Date.h"
+
+
+Date::Date(int year, int month, int day)
+{
+	_year = year;
+	_month = month;
+	_day = day;
+}
+void Date::print()
+{
+	cout << _year << "-" << _month << "-" << _day << endl;
+}
+
+//bool Date::operator<(const Date& d);
+//bool Date::operator<=(const Date& d);
+//bool Date::operator>(const Date& d);
+//bool Date::operator>=(const Date& d);
+//bool Date::operator==(const Date& d);
+//bool Date::operator!=(const Date& d);
+// d1 += 天数​
+Date& Date::operator+=(int day)
+{
+	_day += day;
+
+	while (_day > GetMonthDay(_year, _month))
+	{
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+		if (_month == 13)
+		{
+			_year++;
+			_month = 1;
+		}
+	}
+	return *this;
+}
+Date Date::operator+(int day)
+{
+	Date tmp = *this;
+	tmp += day;
+	return tmp;
+}
---
+++ study/C++/2026.04.16类和对象/Date.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+
+
+class Date
+{
+public:
+	Date(int year = 1970, int month = 1, int day = 1);
+	void print();
+
+	int GetMonthDay(int year, int month)
+	{
+		static int arr[13] = { -1,31,28,31,30,31,30,31,31,30,31,30,31 };
+		// 润年：4年一润、百年不润，400年一润
+		if (month == 2 &&( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+			return 29;
+		else
+			return arr[month];
+	}
+
+	bool operator<(const Date& d);
+	bool operator<=(const Date& d);
+	bool operator>(const Date& d);
+	bool operator>=(const Date& d);
+	bool operator==(const Date& d);
+	bool operator!=(const Date& d);
+	// d1 += 天数​
+	Date& operator+=(int day);
+	Date operator+(int day);
+
+
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
