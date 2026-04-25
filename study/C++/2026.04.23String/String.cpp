c5575ac0fa033843637ce566492deb5502eecc43
uaenawook <820649664@qq.com>
2026-04-25 21:42:51 +0800

    Add:Implementing a String Class

--- study/C++/2026.04.23String/String.cpp
+++ study/C++/2026.04.23String/String.cpp
@@ -2,12 +2,47 @@
 
 namespace wx
 {
+	// Ô¤¶¨ÈÝÁ¿
+	void string::reserve(size_t n)
+	{
+		if (n > _capacity)
+		{
+			char* tmp = new char[n + 1];
+			strcpy(tmp, _str);
+			delete[] _str;
+			_str = tmp;
+			_capacity = n;
+		}
+		
+	}
 
+	void string::push_back(char ch)
+	{
+		if (_size + 1 > _capacity)
+		{
+			reserve(_capacity == 0 ? 4 : _capacity * 2);
+		}
+		_str[_size] = ch;
+		++_size;
+		_str[_size] = '\0';
+	}
 
 	void String_Test1()
 	{
-		string s1;
-		cout << "s1" << endl;
+
+		string s1("hello world");
+		cout << s1.c_str() << endl;
+
+		string s2(s1);
+		cout << s2.c_str() << endl;
+		cout << s2[0] << endl;
+		cout << s2.size() << endl;
+		cout << s2.capacity() << endl;
+
+		s2.push_back('x');
+		cout << s2.c_str() << endl;
+
+
 	}
 
 }
--- study/C++/2026.04.23String/String.h
+++ study/C++/2026.04.23String/String.h
@@ -1,4 +1,6 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include<iostream>
 #include<assert.h>
 
@@ -10,11 +12,97 @@ namespace wx
 	{
 
 	public:
-		string()
-		{}
+		typedef char* iterator;
+		typedef const char* const_iterator;
+
+		iterator begin()
+		{
+			return _str;
+		}
+		iterator end()
+		{
+			return _str + _size;
+		}
+		const_iterator begin() const
+		{
+			return _str;
+		}
+		const_iterator end() const
+		{
+			return _str + _size;
+		}
+		void clear()
+		{
+			_str[0] = '\0';
+			_size = 0;
+		}
+		size_t size() const
+		{
+			return _size;
+		}
+		size_t capacity() const
+		{
+			return _capacity;
+		}
+		char& operator[](size_t pos)
+		{
+			assert(pos <= _size);
+			//return (*this)._str[pos];
+			return _str[pos];
+		}
+		const char& operator[](size_t pos) const
+		{
+			assert(pos <= _size);
+			return _str[pos];
+		}
+
+
+		//string()
+		//	:_str(new char[1]{'\0'})
+		//	,_size(0)
+		//	,_capacity(0)
+		//{}
+		const char* c_str() const
+		{
+			return _str;
+		}
+
+		string(const char* str = "")
+		{
+			_size = strlen(str);
+			_capacity = _size;
+
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+
+		string(string& str)
+		{
+			_size = str._size;
+			_capacity = str._capacity;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str.c_str());
+		}
 
 		~string()
-		{}
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = _capacity = 0;
+		}
+
+
+
+		void reserve(size_t n);
+		void push_back(char ch);
+
+		void append(const char* str);
+		string& operator+=(char ch);
+		string& operator+=(const char* str);
+
+		void insert(size_t pos, char ch);
+		void insert(size_t pos, const char* str);
+		void erase(size_t pos, size_t len = npos);
 
 
 	private:
@@ -22,6 +110,9 @@ namespace wx
 		char* _str;
 		size_t _size;
 		size_t _capacity;
+
+
+		static const size_t npos;
 	};
 
 
