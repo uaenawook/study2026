fbf76c523de771144bf5d83783bd681efb57f049
uaenawook <820649664@qq.com>
2026-05-12 11:33:14 +0800

    Add:Afternoon Task

---
+++ study/C++/2026.05.11继承/2026.5.12Test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+// 2026.5.12
+/*
+	不能被继承的类
+
+	继承和友元
+
+	继承与静态成员
+
+	多继承
+		虚继承
+		菱形继承
+
+	继承和组合
+
+
+	链表的回文：https://leetcode.cn/problems/palindrome-linked-list/solutions/
+		思路：找到中间节点
+			逆置后半段
+			双指针同时走
+*/
+
+
+int main()
+{
+
+	return 0;
+}
